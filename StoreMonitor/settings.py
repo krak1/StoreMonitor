9ad3412c041a552fa050c22c1c41f859a487ef23
krak <55945975+krak1@users.noreply.github.com>
2023-11-03 17:17:15 +0530

    fixed char error

--- StoreMonitor/settings.py
+++ StoreMonitor/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mq&=ptvgq^%ulrn%j3fjhyjta3$d9#+6rzbx5xzsv)*c*v&(mp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
